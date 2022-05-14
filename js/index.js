f0f316e570d08e7ed9b352bd4f4308843706a131
JakobGospoaric1 <jg9837@student.uni-lj.si>
2022-05-14 14:17:28 +0200

    Zemljevid in lokacija

--- js/index.js
+++ js/index.js
@@ -1,116 +1,25 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+var map = L.map('map');
+var tiles;
+var lokacija;
 
-window.addEventListener('DOMContentLoaded', event => {
+navigator.geolocation.getCurrentPosition(createMap);
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+function createMap(position) {
+    map.setView([position.coords.latitude, position.coords.longitude], 20);
 
-    };
+    tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(map);
+}
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
-
-var mapa;
-
-const FRI_LAT = 46.05004;
-const FRI_LNG = 14.46931;
-
-/**
- * Ko se stran naloži, se izvedejo ukazi spodnje funkcije
- */
- window.addEventListener("load", function () {
-    // Osnovne lastnosti mape
-    var mapOptions = {
-        center: [FRI_LAT, FRI_LNG],
-        zoom: 8.5,
-    };
-
-    // Ustvarimo objekt mapa
-    mapa = new L.map("mapa_id", mapOptions);
-
-    // Ustvarimo prikazni sloj mape
-    var layer = new L.TileLayer(
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    );
-
-    // Prikazni sloj dodamo na mapo
-    mapa.addLayer(layer);
-
-    // Ročno dodamo FRI na mapo
-    // dodajMarker(
-    //     FRI_LAT,
-    //     FRI_LNG,
-    //     "Fakulteta za računalništvo in informatiko",
-    //     "FRI"
-    // );
-
-    // izracunajVsoto();
-    // omogociHotele(false);
-
-    // document.getElementById("odrasli").addEventListener("change", (elem) => {
-    //     izracunajVsoto(elem.target.value, 200);
-    // });
-
-    // document.getElementById("otroci").addEventListener("change", (elem) => {
-    //     izracunajVsoto(elem.target.value, 50);
-    // });
-
-    // document.getElementById("nocitev").addEventListener("change", (elem) => {
-    //     izracunajVsoto(elem.target.value);
-    // });
-
-    // document.getElementById("najblizjiHotel")
-    //     .addEventListener("click", function () {
-    //         alert(
-    //             "Najbližji hotel je: " + najblizjiHotelNaziv +
-    //             "\nOddaljen je: " + najblizjiHotelOddaljenost.toFixed(2) + " km."
-    //         );
-    //     });
-
-    // document.getElementById("radij").addEventListener("change", function () {
-    //     prikaziHoteleZnotrajRadija();
-    // });
+map.on('click', function(e) {
+    if(lokacija == null)
+        lokacija = new L.marker(e.latlng).addTo(map);
+    else
+        lokacija.setLatLng(e.latlng);
 });
